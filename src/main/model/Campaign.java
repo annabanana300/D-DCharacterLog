ebb2cf82c3da409ccebd183bb386a7d71fc4ffc5
Anna Fang <afang300@students.cs.ubc.ca>
2025-02-04 23:18:59 -0800

    Added method specifications to Character and Campaign classes

---
+++ src/main/model/Campaign.java
@@ -0,0 +1,38 @@
+package model;
+
+//import java.util.ArrayList;
+
+//represents a D&D campaign showing all characters added
+public class Campaign {
+
+    //EFFECTS: constructs an empty campaign
+    public Campaign() {
+        //ArrayList<Character> characters = new ArrayList<>();
+    }
+
+    //MODIFIES: this
+    //EFFECTS: creates a new character to add to campaign
+    public void addCharacter() {
+
+    }
+
+    //REQUIRES: character already exists in campaign
+    //MODIFIES: this
+    //EFFECTS: removes a character from campaign
+    public void removeCharacter() {
+
+    }
+
+    //EFFECTS: returns (list?) characters added to campaign
+    public void viewCharacters() {
+
+    }
+
+    //REQUIRES: character exists in campaign
+    //MODIFIES: this
+    //EFFECTS: user can edit an attribute of existing character
+    public void editCharacter() {
+
+    }
+
+}
---
+++ src/main/model/Character.java
@@ -0,0 +1,37 @@
+package model;
+
+//import java.util.ArrayList;
+
+//represents a character in a D&D campaign
+public class Character {
+    
+
+    //EFFECTS: constructs a new character
+    public Character() {
+        
+    }
+
+    //setters
+    public void setName() {
+
+    }
+
+    public void setRace() {
+
+    }
+
+    public void setClass() {
+        
+    }
+
+    public void setBackstory() {
+
+    }
+
+
+    //getters
+    public void getName() {
+
+    }
+
+}
