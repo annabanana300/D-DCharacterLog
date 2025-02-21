d95763b72d16dd580773200e90b4e5deb62f575a
Anna Fang <afang300@students.cs.ubc.ca>
2025-02-21 12:39:13 -0800

    Implemented JsonReader class w/ 100% coverage

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -13,38 +13,64 @@ import org.json.*;
 
 //a reader that reads a Campaign from JSON data stored in saved file
 public class JsonReader {
+    private String source; //string of source file to read from
 
     //EFFECTS: constructs reader to read from file
     public JsonReader(String source) {
-
+        this.source = source;
     }
 
     //EFFECTS: reads a Campaign from saved file and returns it
     //throws IOException if data cannot be read
     public Campaign read() throws IOException{
-        return null;
+        String data = readFile(source);
+        JSONObject jsonObject = new JSONObject(data);
+        return parseCampaign(jsonObject);
     }
 
     //reads file as string, returns string
-    private String readFile(String source) {
-        return null;
+    private String readFile(String source) throws IOException{
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> stringBuilder.append(s));
+        }
+        return stringBuilder.toString();
     }
 
     //EFFECTS: parse campaign from JSON object, return it
     private Campaign parseCampaign(JSONObject jsonObject) {
-        //key: "name"
-        return null;
+        Campaign campaign = new Campaign();
+        addCharacters(campaign, jsonObject);
+        return campaign;
     }
 
     //MODIFIES: Campaign c
     //EFFECTS: parse characters from JSON object, add to campaign
     private void addCharacters(Campaign c, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("characters");
+        for (Object json : jsonArray) {
+            JSONObject nextCharacter = (JSONObject) json;
+            addCharacter(c, nextCharacter);
+        }
         //key: "characters"
     }
 
     //MODIFIES: Campaign c
     //EFFECTS: parse character from JSON object, add to campaign
     private void addCharacter(Campaign c, JSONObject jsonObject) {
+        String characterName = jsonObject.getString("name");
+        String race = jsonObject.getString("race");
+        String Characterclass = jsonObject.getString("class");
+        String backstory = jsonObject.getString("backstory");
+        
+        model.Character character = new Character();
 
+        character.setName(characterName);
+        character.setRace(race);
+        character.setClass(Characterclass);
+        character.setBackstory(backstory);
+
+        c.addCharacter(character);
     }
 }
