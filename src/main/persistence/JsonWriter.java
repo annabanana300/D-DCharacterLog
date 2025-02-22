0b8fcb74f11a12563f9dec5d0f71b367784ecd8e
Anna Fang <afang300@students.cs.ubc.ca>
2025-02-22 11:30:08 -0800

    Added specifications for JsonWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,36 @@
+package persistence;
+import model.Campaign;
+import org.json.JSONObject;
+import java.io.*;
+
+public class JsonWriter {
+
+    //EFFECTS: constructs writer
+    public JsonWriter(String destination) {
+        
+    }
+
+    //MODIFIES: this
+    //EFFECTS: open file writer, if file not found/cannot open throw FileNotFoundException
+    public void openWriter() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: write campaign to file in JSON
+    public void write(Campaign campaign) {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: closes writer
+    public void closeWriter() {
+
+    }
+
+    //MODIFIES: this
+    //EFFECTS: print and save JSON string to file
+    private void saveToFile(String jString) {
+        
+    }
+}
